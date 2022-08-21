8f68e62ac60d7140f1a1433b94750ad3afb1f16d
Rayan Ballout <rayan.ballout@eit-mena.com>
2022-08-21 15:42:26 +0300

    Correcting mistakes

--- src/components/aboutHero/AboutHero.js
+++ src/components/aboutHero/AboutHero.js
@@ -7,7 +7,7 @@ const AboutHero = () => {
         <div className="overlay"></div>
         <div className="container about-content">
             <h1>ABOUT US</h1>
-            <p>We are a family of friends, join us for a drink and music and a let the fun vibes fill each moment of your time.
+            <p>We are a family of friends, join us for a drink and music and let the fun vibes fill each moment of your time.
             You are welcome to eat, play and drink in our Hub anytime. Whether you would would like to test your moves against
             other players in a FIFA game or show off your old school Counter Strike skills, Dino's Hub is the place to be. Don't be
             late, we are waiting!</p>
